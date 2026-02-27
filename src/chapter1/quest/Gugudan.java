3b83ae04967ce67cc29adc5991ebfd16acf33bf3
Jaegu <rkdworn@naver.com>
2026-02-27 16:42:43 +0900

    퀘스트3

---
+++ src/chapter1/quest/Gugudan.java
@@ -0,0 +1,12 @@
+package chapter1.quest;
+
+public class Gugudan {
+    public static void main(String[] args) {
+        for (int i =2; i <= 9; i++){
+            System.out.println(i + "단");
+            for (int o = 1; o <= 9; o++){
+                System.out.println(i + "X" + o + "=" + i*o);
+            }
+        }
+    }
+}
