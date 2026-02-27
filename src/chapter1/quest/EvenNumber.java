91f34a6ab924ea1eec71370b327c7f08e5017e21
Jaegu <rkdworn@naver.com>
2026-02-27 16:37:03 +0900

    퀘스트2

---
+++ src/chapter1/quest/EvenNumber.java
@@ -0,0 +1,14 @@
+package chapter1.quest;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        int evenNumber = 100;
+        for (int i = 1; i <= evenNumber; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+
+
+            }
+        }
+    }
+}
