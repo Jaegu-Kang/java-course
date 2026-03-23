bd1e3ac1577073fda723151f933e53c0acc891ee
Jaegu-Kang <rkdworn8@gmail.com>
2026-03-23 14:20:45 +0900

    계산기 step1

--- src/chapter1_1/calculator/Main.java
+++ src/chapter1_1/calculator/Main.java
@@ -7,29 +7,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("첫번째 숫자를 입력하세요: ");
+            int a = sc.nextInt();
+            System.out.println("두번째 숫자를 입력하세요: ");
+            int b = sc.nextInt();
+            sc.nextLine();
+            System.out.println("사칙연산 기호를 입력하세요: (종료하려면 exit를 입력하세요) ");
+            String c = sc.nextLine();
 
-        System.out.println("첫번째 숫자를 입력하세요: ");
-        int a = sc.nextInt();
-        System.out.println("두번째 숫자를 입력하세요: ");
-        int b = sc.nextInt();
-        sc.nextLine();
-        System.out.println("사칙연산 기호를 입력하세요: ");
-        String c = sc.nextLine();
-        int sum = a + b;
-        int sub = a - b;
-        int mul = a * b;
-        int div = a / b;
+            if (c.equals("exit")){
+                System.out.println("계산기를 종료합니다.");
+                break;
+            }
+            else if (c.equals("+")) {
+                System.out.println("결과 = " + (a + b));
+            } else if (c.equals("-")) {
+                System.out.println("결과 = " + (a - b));
+            } else if (c.equals("*")) {
+                System.out.println("결과 = " + (a * b));
+            } else if (c.equals("/")) {
+                System.out.println("결과 = " + (a - b));
+            }
 
-        if (c.equals("+")){
-            System.out.println("결과 = " + sum);
-        } else if (c.equals("-")) {
-            System.out.println("결과 = " + sub);
-        } else if (c.equals("*")) {
-            System.out.println("결과 = " + mul);
-        } else if (c.equals("/")) {
-            System.out.println("결과 = " + div);
+
         }
-
-
     }
 }
